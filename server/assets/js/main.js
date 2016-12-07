b7dbede3f67659f028fda5346d21e9b9954ad361
Corw <pzamyatin@gmail.com>
2016-12-07 18:08:06 +1300

    Absolutely new interface using bootstrap

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -2,19 +2,50 @@ $(document).ready(function() {
   console.log('main.js is ready');
 
   // show ALL data in main block
-  $('#show-button').click(function() {
+  $('#show-all').click(function() {
     $.ajax({
       type: 'GET',
       url: '/api/entries',
       success: function(array) {
-        // console.log(array);
+        console.log(array);
         var newHTML = $.map(array, function(value) {
-          return ('<p>' + value.user + ' ' + value.sum + ' ' + value.date + '</p>');
+          return (`
+            <tr>
+              <td>${value._id}</td>
+              <td>${value.user}</td>
+              <td>${value.sum}</td>
+              <td>${value.category}</td>
+            </tr>
+            `);
         });
-        $("#data").html(newHTML.join(""));
+        $("#main-table").html(newHTML.join(""));
       },
       error: function(err){
         console.log(err);
+      }
+    });
+  });
+
+  // POST NEW ENTRY
+  $('#post').click(function() {
+    var arr = JSON.stringify(['fuck', 'you', 'bitch']);
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/entries',
+      dataType: "json",
+      data: {
+        user      : "ajax",
+        date      : new Date(),
+        sum       : 88.88,
+        category  : arr,
+        comment   : "Post request from ajax"
+      },
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err){
+        alert(err);
       }
     });
   });
