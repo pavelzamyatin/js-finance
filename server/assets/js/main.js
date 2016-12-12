e2286249415dea560bd0030b76b530e877065b4c
Corw <pzamyatin@gmail.com>
2016-12-12 19:57:14 +1300

    Added SHOW ID button

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -1,33 +1,6 @@
 $(document).ready(function() {
   console.log('main.js is ready');
 
-  // GET ALL data in main block
-  $('#show-all-button').click(function() {
-    $.ajax({
-      type: 'GET',
-      url: '/api/entries',
-      success: function(response) {
-        console.log(response);
-        var newHTML = $.map(response.ITEMS, function(value) {
-          return (`
-            <tr>
-              <td>${value._id}</td>
-              <td>${value.user}</td>
-              <td>${value.sum}</td>
-              <td>${value.category}</td>
-              <td>${value.date}</td>
-              <td>${value.comment}</td>
-            </tr>
-            `);
-        });
-        $("#main-table").html(newHTML.join(""));
-      },
-      error: function(err){
-        console.log(err);
-      }
-    });
-  });
-
   // POST FROM form
   $('form').on('submit', function(e) {
     var formData = $('form').serializeArray();
@@ -55,6 +28,52 @@ $(document).ready(function() {
     return false;
   })
 
+  // =======================================
+  // ============== TEST ZONE ==============
+  // =======================================
+
+  // This function helps to show data in the table from the API GET request
+  var showTable = function(res) {
+    console.log(res);
+    var newHTML = $.map(res.ITEMS, function(value) {
+      return (`
+        <tr>
+          <td>${value._id}</td>
+          <td>${value.user}</td>
+          <td>${value.sum}</td>
+          <td>${value.category}</td>
+          <td>${value.date}</td>
+          <td>${value.comment}</td>
+        </tr>
+        `);
+    });
+    $("#main-table").html(newHTML.join(""));
+  };
+
+  // GET all entries
+  $('#show-all-button').click(function() {
+    $.ajax({
+      type: 'GET',
+      url: '/api/entries',
+      success: function(response) { showTable(response) },
+      error: function(err){
+        console.log(err);
+      }
+    });
+  });
+
+  // GET entry by ID
+  $('#show-id-button').click(function() {
+    $.ajax({
+      type: 'GET',
+      url: '/api/entry/' + $('#inputId')[0].value,
+      success: function(response) { showTable(response) },
+      error: function(err){
+        console.log(err);
+      }
+    });
+  });
+
   // POST NEW ENTRY BUTTON TEST
   $('#post-entry-button').click(function() {
 
@@ -73,7 +92,7 @@ $(document).ready(function() {
         console.log(data);
       },
       error: function(err){
-        alert(err);
+        console.log(err);
       }
     });
   });
