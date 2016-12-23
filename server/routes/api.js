8ded4b9da59fa448bc9053a4a3b76ec800fc6fa9
Corw <pzamyatin@gmail.com>
2016-12-23 20:34:43 +1300

    Added filters to api - date, dateFrom, dateTo and sort update #5

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,22 @@
 var express     = require('express');
 var router      = express.Router();
-var validator   = require('validator');
 var Entry       = require('../models/entry');
+
+// addition modules
+var validator   = require('validator');
+var moment      = require('moment');
 
 // *** protect from CSRF *** ///
 var csrf            = require('csurf');
 var csrfProtection  = csrf({ cookie: true })
+
+// =========================================================================
+// HELP METHODS ============================================================
+// =========================================================================
+var dateFormat = function(date, format) {
+    var validDate = validator.toDate(date);
+    return moment(validDate).format(format);
+}
 
 // =========================================================================
 // API ROUTES =============================================================
@@ -13,29 +24,49 @@ var csrfProtection  = csrf({ cookie: true })
 
 // *** get ALL Entries plus FILTERED Entries ***
 router.get('/api/entries', function(req, res) {
-  // create new instance of Entry model
-  var newEntry = Entry.find({})
+    // create new instance of Entry model
+    var newEntry = Entry.find({})
 
-  // checking filters
-  if(req.query.category) {
-    newEntry.where('category').equals(req.query.category);
-  }
+    // ********** FILTERS *************
 
-  // response generator
-  newEntry.exec(function(err, entries) {
-      if(err) {
-        res.json({
-            "STATUS": "ERROR",
-            "ERROR": err,
-            "ITEMS": []
-        });
-      } else {
-        res.json({
-            "STATUS": "SUCCESS",
-            "ERROR": "",
-            "ITEMS": entries
-        });
-      }
+    // category filter
+    if(req.query.category) {
+        newEntry.where('category').equals(req.query.category);
+    }
+
+    // date filter
+    if(req.query.date) {
+        newEntry.where('date').equals(dateFormat(req.query.date, 'L'));
+    }
+
+    // dateFrom filter
+    if(req.query.dateFrom) {
+        newEntry.where('date').gt(dateFormat(req.query.dateFrom, 'L'));
+    }
+
+    // dateTo filter
+    if(req.query.dateTo) {
+        newEntry.where('date').lt(dateFormat(req.query.dateTo, 'L'));
+    }
+
+    // sort
+    newEntry.sort('-date');
+
+    // ********** RESPONSE GENERATOR *************
+    newEntry.exec(function(err, entries) {
+        if(err) {
+            res.json({
+                "STATUS": "ERROR",
+                "ERROR": err,
+                "ITEMS": []
+            });
+        } else {
+            res.json({
+                "STATUS": "SUCCESS",
+                "ERROR": "",
+                "ITEMS": entries
+            });
+        }
     });
 });
 
